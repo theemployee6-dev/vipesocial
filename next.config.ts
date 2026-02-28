8c0a73b6cdbb1294edf80e1513a912561002b8d7
theemployee6-dev <theemployee6@gmail.com>
2026-02-27 23:32:37 -0300

    alterei - next.config | package.json

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
   /* config options here */
   allowedDevOrigins: ["localhost:3001", "192.168.15.109"],
   webpack: (config) => {
