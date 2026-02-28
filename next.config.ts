3b0e5534409dc6a088b040b842246627b0a74a47
theemployee6-dev <theemployee6@gmail.com>
2026-02-27 23:36:16 -0300

    comentei webpack

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,14 @@ const nextConfig: NextConfig = {
   turbopack: {},
   /* config options here */
   allowedDevOrigins: ["localhost:3001", "192.168.15.109"],
-  webpack: (config) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     asyncWebAssembly: true,
+  //     layers: true,
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
