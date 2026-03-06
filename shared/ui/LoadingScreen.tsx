be2912cc2e5d9e93e9e2779fb900fa10391bb0af
theemployee6-dev <theemployee6@gmail.com>
2026-03-06 20:38:11 -0300

    fixed img filename

--- shared/ui/LoadingScreen.tsx
+++ shared/ui/LoadingScreen.tsx
@@ -260,7 +260,7 @@ export default function LoadingScreen({
           position: relative;
           width: clamp(140px, 20vw, 520px);
           height: clamp(178px, 25vw, 660px);
-          -webkit-mask-image: url('/assets/img/logo/logo_v_sem_fundo.png');
+          -webkit-mask-image: url('./assets/img/logo/logo_v_sem_fundo.png');
           mask-image: url('/assets/img/logo/logo_v_sem_fundo.png');
           -webkit-mask-size: contain;
           mask-size: contain;
