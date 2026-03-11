5d3a250ac1ad428a08ae3464d262a88f12eabc41
theemployee6-dev <theemployee6@gmail.com>
2026-03-11 17:11:13 -0300

    fixed formClass

--- app/auth/register/styles/formClasses.ts
+++ app/auth/register/styles/formClasses.ts
@@ -4,7 +4,7 @@ export const inputClass = `
   px-4 py-2.5
   sm:px-5 sm:py-3
   lg:px-10 lg:py-3
-  xl:px-10 xl:py-3
+  xl:px-10 xl:py-3.5
   2xl:px-10 2xl:py-4
 
   rounded-xl
@@ -27,7 +27,7 @@ export const selectClass = `
   px-3 py-2.5
   sm:px-4 sm:py-3
   lg:px-5 lg:py-2.5
-  xl:px-6 xl:py-3.5
+  xl:px-10 xl:py-3.5
   2xl:px-8 2xl:py-4
 
   rounded-xl
