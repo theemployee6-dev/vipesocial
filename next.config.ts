d6f6aedb40b729b59ce2758167bbaaf35247d135
theemployee6-dev <theemployee6@gmail.com>
2026-03-13 17:01:33 -0300

    fixed bodySizeLimit to 5mb

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["localhost:3000", "192.168.15.202"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
 };
 
 export default nextConfig;
